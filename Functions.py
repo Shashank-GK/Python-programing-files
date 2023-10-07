90ef28dc45ba1ef7da94e84fd4c56d9e9442117e
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2023-10-07 20:46:17 +0530

    commit

--- Functions.py
+++ Functions.py
@@ -8,3 +8,15 @@ N1=float(input('Enter the 1st number:'))
 N2=float(input('Enter the 2nd number:'))
 
 print(sum(N1,N2))
+
+# Calculating the net sealery of an employs
+
+def net_sal(basic,allowance,deduction):
+    net=basic+allowance-deduction
+    return net
+
+basic_Sealery=float(input('Enter the basic sealery of employ: '))
+Allowance=float(input('Enter the allowance: '))
+Deduction=float(input('Enter the amount of deduction: '))
+
+print('Your net sealery is ',net_sal(basic_Sealery,Allowance,Deduction))
