e39391dc1fec623fe3365ef83f254e35529cb584
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2023-10-27 09:10:39 +0530

    Commit

--- File Handling/fileHandling.py
+++ File Handling/fileHandling.py
@@ -1,4 +1,19 @@
 # This program is used to demonstrate file handling in python
+
+# Writing into the file
+try:
+    file = open(
+        r"E:\GitHub\Python-programing-files\File Handling\MyData.txt",
+        "w",
+        encoding="utf-8",
+    )
+    file.write(
+        "\n “Life is a matter of choices, and every choice you make makes you”- John C Maxwell. \n"
+    )
+finally:
+    file.close()
+
+# Reading a file
 try:
     file = open(
         r"E:\GitHub\Python-programing-files\File Handling\MyData.txt",
