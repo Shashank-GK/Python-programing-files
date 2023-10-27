f2f086c6c150b99245735318b1b759f68f8c409e
Shashank GK <89540267+Shashank-GK@users.noreply.github.com>
2023-10-27 08:47:28 +0530

    Commit

--- File Handling/fileHandling.py
+++ File Handling/fileHandling.py
@@ -1,7 +1,10 @@
 # This program is used to demonstrate file handling in python
-
-file = open(
-    r"E:\GitHub\Python-programing-files\File Handling\File.txt", "r", encoding="utf-8"
-)
-print(file.read())
-file.close()
+try:
+    file = open(
+        r"E:\GitHub\Python-programing-files\File Handling\MyData.txt",
+        "r",
+        encoding="utf-8",
+    )
+    print(file.read())
+finally:
+    file.close()
